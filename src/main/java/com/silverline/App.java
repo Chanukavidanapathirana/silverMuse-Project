6f78bf4bb3c05d9e754ae76da436f6942531ce41
Chanukavidanapathirana <chanukasuboth1@gmail.com>
2022-08-13 12:44:53 +0530

    First commit

---
+++ src/main/java/com/silverline/App.java
@@ -0,0 +1,13 @@
+package com.silverline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
